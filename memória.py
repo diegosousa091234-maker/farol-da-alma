a9157d93b8df7f75587906dc8cc6f0335ad40eea
diegosousa091234-maker <diegosousa091234@gmail.com>
2026-08-27 02:29:52 -0300

    Delete memória.py

--- memória.py
+++
@@ -1,8 +0,0 @@
-memorias_db = {}
-def salvar_memoria(usuario: str, texto: str):
-    if usuario not in memorias_db:
-        memorias_db[usuario] = []
-    memorias_db[usuario].append({"tipo": "conversa", "texto": texto})
-    return True
-def buscar_memoria(usuario: str):
-    return memorias_db.get(usuario, [])
