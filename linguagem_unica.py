da64e01c3ea74c53bdf89cb63ba8598f4286dfdf
diegosousa091234-maker <diegosousa091234@gmail.com>
2026-08-27 05:42:25 -0300

    Delete linguagem_unica.py

--- linguagem_unica.py
+++
@@ -1,2 +0,0 @@
-def gerar_resposta(nome: str):
-    return f"{nome}, quando tudo parece escuro, existe uma luz dentro de voce esperando pra brilhar. O Senhor e a minha luz e a minha salvacao - Salmos 27:1. O Farol da Alma esta aceso por voce."
